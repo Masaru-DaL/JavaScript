47c2d991028bcbdec6da3bd566b6bdda246286a4
Masaru-create <hittec9609@icloud.com>
2022-04-12 22:17:55 +0900

    PullDown Step2(switch)

--- JavaScriptCode/PullDown/script.js
+++ JavaScriptCode/PullDown/script.js
@@ -2,12 +2,24 @@
 
 const lang = document.querySelector('html').lang;
 
-if (lang === 'ja') {
+/* if (lang === 'ja') {
   document.querySelector('option[value="index.html"]').selected = true;
 } else if (lang === 'en') {
   document.querySelector('option[value="index_en.html"]').selected = true;
 } else if (lang === 'zh') {
   document.querySelector('option[value="index_zh.html"]').selected = true;
+} */
+
+switch (lang) {
+  case 'ja':
+    document.querySelector('option[value="index.html"]').selected = true;
+    break;
+  case 'en':
+    document.querySelector('option[value="index_en.html"]').selected = true;
+    break;
+  case 'zh':
+    document.querySelector('option[value="index_zh.html"]').selected = true;
+    break;
 }
 
 document.getElementById('form').select.onchange = function() {
